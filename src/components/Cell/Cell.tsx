b5d7cb5498609cc751c37ae705fecfebe1d0d838
DESKTOP-7DGGFV2\Dovran <sony.energizer@gmail.com>
2022-02-09 11:34:55 +0500

    Cell component was connected to the state and styles

--- src/components/Cell/Cell.tsx
+++ src/components/Cell/Cell.tsx
@@ -1,24 +1,36 @@
+import { GameActionType, GameStatus } from 'assets/constants';
+import { useGameDispatcher } from 'hooks/useGameDispatcher';
+import { useGameState } from 'hooks/useGameState';
+import React from 'react';
 import classes from './Cell.module.css';
 
 interface CellProps {
   x: number;
   y: number;
   value: string|null;
-  clickHandler: (row: number, column: number) => void
 }
 
 function Cell(props: CellProps): JSX.Element {
-  const { x, y, value, clickHandler} = props;
+  const { status } = useGameState();
+  const { x, y, value } = props;
+  const dispatcher = useGameDispatcher();
 
-  const onClick = (e: React.MouseEvent): void => {
-    clickHandler(x, y);
+  const onClick: React.MouseEventHandler = e => {
+    dispatcher({
+      type: GameActionType.UPDATE,
+      payload: { x, y }
+    });
   }
+  
+  const disabled = React.useMemo(() => {
+    return value !== null || status === GameStatus.OVER;
+  }, [value, status]);
 
   return (
     <button 
       onClick={onClick}
       className={classes['cell']}
-      disabled={value !== null}
+      disabled={disabled}
     >
       { value }
     </button>
