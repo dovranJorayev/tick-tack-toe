15e4aab2a4fd6f629fa8bb06726d47c1f9882eeb
DESKTOP-7DGGFV2\Dovran <sony.energizer@gmail.com>
2022-02-09 11:32:07 +0500

    Added types for entites in state

---
+++ src/state/types.ts
@@ -0,0 +1,27 @@
+import { GameActionType, GameRole, GameStatus } from 'assets/constants';
+
+export type Cell = null | GameRole;
+export type Row = Cell[];
+export type Table = Row[];
+
+export interface GameState {
+  table: Table;
+  currentRole: GameRole;
+  winner: GameRole | null;
+  status: GameStatus;
+}
+
+export interface GameContextValue {
+  state: GameState;
+  dispatch: React.Dispatch<GameAction>;
+}
+
+export interface Dimension {
+  x: number;
+  y: number;
+}
+
+export interface GameAction {
+  type: GameActionType;
+  payload?: Dimension
+}
